93c0058eb11652e13c0e4f9a5f45c277a1766e65
prabhat <kapkotiprabhat@gmail.com>
2026-07-02 16:46:57 +0530

    Add styling to pizza app with external CSS

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import "./index.css";
 
+// eslint-disable-next-line no-unused-vars
 const pizzaData = [
   {
     name: "Focaccia",
@@ -49,10 +51,10 @@ const pizzaData = [
 // this is a component
 function App() {
   return (
-    <div>
-      <h1>Hello React!!!!</h1>
-      <Pizza />
-      <Pizza />
+    <div className="container">
+      <Header />
+      <Menu />
+      <Footer />
     </div>
   );
 }
@@ -61,9 +63,52 @@ function Pizza() {
   return (
     <div>
       <img src="pizzas/spinaci.jpg" alt="pizaa Spinaci" />
-      <h2>Pizza Spinaci</h2>
+      <h3>Pizza Spinaci</h3>
       <p>Tomato, mozarella, spinach, and ricotta cheese</p>
     </div>
+  );
+}
+
+function Header() {
+  // const style = { color: "red", fontSize: "48px", textTransform: "Uppercase" };
+
+  const style = {};
+
+  return (
+    <header className="header">
+      <h1 style={style}> Fast React Pizza Co.</h1>;
+    </header>
+  );
+}
+
+function Menu() {
+  return (
+    <main className="menu">
+      <h2>Our menu</h2>
+      <Pizza />
+      <Pizza />
+      <Pizza />
+      <Pizza />
+    </main>
+  );
+}
+
+function Footer() {
+  const hour = new Date().getHours();
+  const openHour = 12;
+  const closeHour = 22;
+
+  const isOpen = hour >= openHour && hour <= closeHour;
+  console.log(isOpen);
+
+  // if (hour >= openHour && hour <= closeHour) alert(`We're currently open!`);
+  // else alert(`Sorry we're close`);
+  // console.log(hour);
+
+  return (
+    <footer className="footer">
+      {new Date().toLocaleTimeString()}. We're currently open
+    </footer>
   );
 }
 
